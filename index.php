d85c0f271a774a8d95f8f9582ebcf267761e8319
José Iván Chapoñán Casiano <ivan@navike21.com>
2018-06-30 12:21:07 -0500

    subiendo archivos

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Ponte Flaca</title>
+
+        <link rel="stylesheet" href="http://bastemp.com/css/bastemp.min.css?ver=1.1.2">
+        <link rel="stylesheet" href="css/styles.min.css">
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" charset="utf-8"></script>
+        <script src="http://bastemp.com/js/bastemp.min.js?ver=1.1.2" charset="utf-8"></script>
+        <script src="js/ready.min.js" charset="utf-8"></script>
+
+    </head>
+    <body class="section_top_center">
+
+    </body>
+</html>
